1b69688e8e20edb99a1f7c2faff01046038737b1
Chen1996-1 <2435740021@qq.comm>
2021-05-29 14:09:19 +0800

    init project

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+// vue.config.js
+module.exports = {
+  css: {
+    loaderOptions: {
+      // 给 sass-loader 传递选项
+      sass: {
+        // @/ 是 src/ 的别名
+        // 所以这里假设你有 `src/variables.sass` 这个文件
+        // 注意：在 sass-loader v8 中，这个选项名是 "prependData"
+        additionalData: `
+        @import "~@/assets/scss/variables.sass";
+        @import "~@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  }
+}
