689276a11552c9ac869e28488fe272dfc645aad5
Chen1996-1 <2435740021@qq.comm>
2021-05-29 16:58:24 +0800

    全局引入样式, vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,13 @@
 module.exports = {
   css: {
     loaderOptions: {
-      // 给 sass-loader 传递选项
       sass: {
-        // @/ 是 src/ 的别名
-        // 所以这里假设你有 `src/variables.sass` 这个文件
-        // 注意：在 sass-loader v8 中，这个选项名是 "prependData"
-        additionalData: `
-        @import "~@/assets/scss/variables.sass";
-        @import "~@/assets/scss/mixin.scss";
+        // 全局引入变量和 mixin
+        prependData:
         `
+        @import "@/assets/scss/variable.scss";
+        @import "@/assets/scss/mixin.scss";
+      `
       }
     }
   }
